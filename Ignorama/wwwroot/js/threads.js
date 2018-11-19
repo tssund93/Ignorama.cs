9cc7986d03d024de625bd66d7487ecac57ffaeba
Travis <tssund93@gmail.com>
2018-11-19 17:23:54 -0600

    populate tags on new thread page from model

---
+++ Ignorama/wwwroot/js/threads.js
@@ -0,0 +1,8 @@
+﻿new Vue({
+    el: 'main',
+    data: { threads: [] },
+    created: function () {
+        axios.get('/Threads/GetThreads')
+            .then(response => this.threads = response.data);
+    },
+});
