944ff7d592c1c193d9a36dce0a8f621bd5bb7281
Travis <tssund93@gmail.com>
2018-11-19 16:17:41 -0600

    permission levels table

---
+++ Ignorama/Migrations/20181119221644_AddPermissionLevels.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Ignorama.Migrations
+{
+    public partial class AddPermissionLevels : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "RequiredPermissionLevel",
+                table: "Tags",
+                newName: "RequiredPermissionLevelID");
+
+            migrationBuilder.CreateTable(
+                name: "PermissionLevels",
+                columns: table => new
+                {
+                    ID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(nullable: false),
+                    Level = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PermissionLevels", x => x.ID);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Tags_RequiredPermissionLevelID",
+                table: "Tags",
+                column: "RequiredPermissionLevelID");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Tags_PermissionLevels_RequiredPermissionLevelID",
+                table: "Tags",
+                column: "RequiredPermissionLevelID",
+                principalTable: "PermissionLevels",
+                principalColumn: "ID",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Tags_PermissionLevels_RequiredPermissionLevelID",
+                table: "Tags");
+
+            migrationBuilder.DropTable(
+                name: "PermissionLevels");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Tags_RequiredPermissionLevelID",
+                table: "Tags");
+
+            migrationBuilder.RenameColumn(
+                name: "RequiredPermissionLevelID",
+                table: "Tags",
+                newName: "RequiredPermissionLevel");
+        }
+    }
+}
