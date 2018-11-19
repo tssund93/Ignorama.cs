1106ca7fb001b5dbf7bfd5dd7ffd13417695d5e9
Travis <tssund93@gmail.com>
2018-11-18 21:18:48 -0600

    get threads from database

---
+++ Ignorama/Controllers/ThreadsController.cs
@@ -0,0 +1,158 @@
+﻿using Ignorama.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Ignorama.Controllers
+{
+    public class ThreadsController : Controller
+    {
+        private readonly ForumContext _context;
+
+        public ThreadsController(ForumContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Threads
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Threads.ToListAsync());
+        }
+
+        // GET: Threads/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var thread = await _context.Threads
+                .FirstOrDefaultAsync(m => m.ID == id);
+            if (thread == null)
+            {
+                return NotFound();
+            }
+
+            return View(thread);
+        }
+
+        // GET: Threads/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Threads/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("ID,Title,Stickied,Locked,Deleted")] Thread thread)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(thread);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(thread);
+        }
+
+        // GET: Threads/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var thread = await _context.Threads.FindAsync(id);
+            if (thread == null)
+            {
+                return NotFound();
+            }
+            return View(thread);
+        }
+
+        // POST: Threads/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("ID,Title,Stickied,Locked,Deleted")] Thread thread)
+        {
+            if (id != thread.ID)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(thread);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ThreadExists(thread.ID))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(thread);
+        }
+
+        // GET: Threads/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var thread = await _context.Threads
+                .FirstOrDefaultAsync(m => m.ID == id);
+            if (thread == null)
+            {
+                return NotFound();
+            }
+
+            return View(thread);
+        }
+
+        // POST: Threads/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var thread = await _context.Threads.FindAsync(id);
+            _context.Threads.Remove(thread);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ThreadExists(int id)
+        {
+            return _context.Threads.Any(e => e.ID == id);
+        }
+
+        [HttpGet]
+        public IActionResult GetThreads()
+        {
+            return new OkObjectResult(_context.Threads.ToList());
+        }
+    }
+}
