f5e43cb2d5e53b61068e9d08ae95705238a46901
Travis Sunderland <travis@sunderland.io>
2018-11-19 20:19:57 -0600

    hide thread ability in frontend

--- Ignorama/wwwroot/js/threads.js
+++ Ignorama/wwwroot/js/threads.js
@@ -1,15 +1,31 @@
-﻿new Vue({
+﻿var threadsVue = new Vue({
     el: 'main',
     data: { threads: [] },
     created: function () {
         axios.get('/Threads/GetThreads')
-            .then(response => this.threads = response.data);
+            .then(response => {
+                this.threads = response.data.map(thread =>
+                    ({
+                        ...thread,
+                        hidden: false
+                    }));
+            });
     },
     filters: {
         date: function (date) {
             if (!date) return '';
             date = new Date(date);
             return date.toLocaleString(); 
+        }
+    },
+    computed: {
+        visibleThreads: function () {
+            return this.threads.filter(thread => !thread.hidden);
+        }
+    },
+    methods: {
+        hideThread: function (thread) {
+            thread.hidden = true
         }
     }
 });
