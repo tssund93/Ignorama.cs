440e352999280f267b6af79f966817f921615eaf
Travis Sunderland <travis@sunderland.io>
2018-11-21 13:33:39 -0600

    show logged in username and logout link when logged in

--- Ignorama/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ Ignorama/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using Ignorama.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.UI.Services;
 using Microsoft.AspNetCore.Mvc;
@@ -15,14 +16,14 @@ namespace Ignorama.Areas.Identity.Pages.Account
     [AllowAnonymous]
     public class RegisterModel : PageModel
     {
-        private readonly SignInManager<Ignorama.Models.User> _signInManager;
-        private readonly UserManager<Ignorama.Models.User> _userManager;
+        private readonly SignInManager<User> _signInManager;
+        private readonly UserManager<User> _userManager;
         private readonly ILogger<RegisterModel> _logger;
         private readonly IEmailSender _emailSender;
 
         public RegisterModel(
-            UserManager<Ignorama.Models.User> userManager,
-            SignInManager<Ignorama.Models.User> signInManager,
+            UserManager<User> userManager,
+            SignInManager<User> signInManager,
             ILogger<RegisterModel> logger,
             IEmailSender emailSender)
         {
@@ -66,7 +67,7 @@ namespace Ignorama.Areas.Identity.Pages.Account
             returnUrl = returnUrl ?? Url.Content("~/");
             if (ModelState.IsValid)
             {
-                var user = new Ignorama.Models.User { UserName = Input.UserName };
+                var user = new User { UserName = Input.UserName };
                 var result = await _userManager.CreateAsync(user, Input.Password);
                 if (result.Succeeded)
                 {
