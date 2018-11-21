1df300dd2b0a7c0304cd596e5a3b70617a992d44
Travis <tssund93@gmail.com>
2018-11-21 14:30:42 -0600

    add ip to posts

---
+++ Ignorama/Migrations/20181121202923_AddIPToPosts.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Ignorama.Migrations
+{
+    public partial class AddIPToPosts : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Posts_AspNetUsers_UserId",
+                table: "Posts");
+
+            migrationBuilder.AlterColumn<long>(
+                name: "UserId",
+                table: "Posts",
+                nullable: true,
+                oldClrType: typeof(long));
+
+            migrationBuilder.AddColumn<string>(
+                name: "IP",
+                table: "Posts",
+                nullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Posts_AspNetUsers_UserId",
+                table: "Posts",
+                column: "UserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Posts_AspNetUsers_UserId",
+                table: "Posts");
+
+            migrationBuilder.DropColumn(
+                name: "IP",
+                table: "Posts");
+
+            migrationBuilder.AlterColumn<long>(
+                name: "UserId",
+                table: "Posts",
+                nullable: false,
+                oldClrType: typeof(long),
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Posts_AspNetUsers_UserId",
+                table: "Posts",
+                column: "UserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
