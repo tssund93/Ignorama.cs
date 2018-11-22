7335da73404b365c9cab26be018e55c2788bde9d
Travis <tssund93@gmail.com>
2018-11-22 12:25:13 -0600

    persist selected tags

--- Ignorama/Controllers/TagsController.cs
+++ Ignorama/Controllers/TagsController.cs
@@ -27,5 +27,64 @@ namespace Ignorama.Controllers
                 _context.Tags
                     .OrderBy(tag => tag.Name));
         }
+
+        public async Task<IActionResult> GetSelectedTags()
+        {
+            var user = await _userManager.GetUserAsync(User);
+            return new OkObjectResult(
+                user != null
+                    ? _context.SelectedTags
+                        .Where(tag => tag.User == user)
+                        .Select(tag => tag.Tag.ID)
+                        .ToList()
+                    : _context.SelectedTags
+                        .Where(tag => tag.IP == Request.HttpContext.Connection.RemoteIpAddress.ToString())
+                        .Select(tag => tag.Tag.ID)
+                        .ToList());
+        }
+
+        public class TagIDModel
+        {
+            public int TagID { get; set; }
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> ToggleSelectedTag([FromBody] TagIDModel t)
+        {
+            var user = await _userManager.GetUserAsync(HttpContext.User);
+
+            var tag = _context.Tags.Find(t.TagID);
+
+            if (tag != null)
+            {
+                var selectedTagRows = user != null
+                    ? _context.SelectedTags.Where(st => st.User == user && st.Tag == tag)
+                    : _context.SelectedTags.Where(
+                        st => st.IP == Request.HttpContext.Connection.RemoteIpAddress.ToString() && st.Tag == tag);
+
+                if (!selectedTagRows.Any())
+                {
+                    var selectedTag = new SelectedTag
+                    {
+                        Tag = tag,
+                        User = user,
+                        IP = user == null
+                            ? Request.HttpContext.Connection.RemoteIpAddress.ToString()
+                            : null
+                    };
+
+                    await _context.AddAsync(selectedTag);
+                }
+                else
+                {
+                    foreach (SelectedTag row in selectedTagRows)
+                        _context.Remove(row);
+                }
+                await _context.SaveChangesAsync();
+
+                return new OkObjectResult(t.TagID);
+            }
+            else return new BadRequestObjectResult(tag);
+        }
     }
 }
