5d4f35ad688755f6fce2103769b0ada8a4283a81
Travis <tssund93@gmail.com>
2018-11-23 01:19:59 -0600

    only allow whitelisted file types

--- Ignorama/wwwroot/js/uploadfile.js
+++ Ignorama/wwwroot/js/uploadfile.js
@@ -1,14 +1,14 @@
-$(document).ready(function() {
-    $(function(){
-        $('#uploadForm').submit(function(){
+$(document).ready(function () {
+    $(function () {
+        $('#uploadForm').submit(function () {
             $('<input type="hidden" name="javascript" value="yes" />').appendTo($(this));
             var iframeName = ('iframeUpload');
-            var iframeTemp = $('<iframe id="uploadframe" name="'+iframeName+'" src="about:blank" />');
+            var iframeTemp = $('<iframe id="uploadframe" name="' + iframeName + '" src="about:blank" />');
             iframeTemp.css('display', 'none');
             $('body').append(iframeTemp);
             /* submit the uploadForm */
             $(this).attr({
-                action: 'b/upload-file',
+                action: '/Home/UploadFile',
                 method: 'post',
                 enctype: 'multipart/form-data',
                 encoding: 'multipart/form-data',
@@ -18,10 +18,10 @@ $(document).ready(function() {
             var timeout = 60;
             var postcontents = document.getElementById('postfield').value;
             var uploadingtext = postcontents + " Uploading file";
-            var uploadinterval = setInterval(function(){
+            var uploadinterval = setInterval(function () {
                 var error = document.getElementById("uploadframe").contentWindow.error;
-                if (typeof error !== 'undefined' || count > timeout){
-                    if (count > timeout){
+                if (typeof error !== 'undefined' || count > timeout) {
+                    if (count > timeout) {
                         document.getElementById('postfield').value = postcontents;
                         alert("Upload timed out.  Your file is probably too big. (Max: 10 MB)");
                         document.getElementById("submitbutton").disabled = false;
@@ -31,25 +31,25 @@ $(document).ready(function() {
                     iframeTemp.remove();
                     document.getElementById("submitbutton").disabled = false;
                 }
-                else{
+                else {
                     document.getElementById('postfield').value = uploadingtext + ".";
                     uploadingtext = document.getElementById('postfield').value;
                 }
                 $('input[name="javascript"]').remove();
                 inputLength = 0;
-                inputLength += $('input[name="upload"]').val().length;
-                if(0 < inputLength && error === 'none'){
-                    $('input[name="upload"]').val('');
+                inputLength += $('input[name="file"]').val().length;
+                if (0 < inputLength && error === 'none') {
+                    $('input[name="file"]').val('');
                     document.getElementById('postfield').value = postcontents;
                     temp = $('#filename').val().split(".");
-                    extension = temp[temp.length-1];
-                    if (extension === 'webm'){
+                    extension = temp[temp.length - 1];
+                    if (extension === 'webm') {
                         document.getElementById('postfield').value += "[webm]" +
-                            window.location.origin+'/uploads/'+$('#filename').val()+"[/webm]";
+                            window.location.origin + '/uploads/' + $('#filename').val() + "[/webm]";
                     }
-                    else{
+                    else {
                         document.getElementById('postfield').value += "[img]" +
-                            window.location.origin+'/uploads/'+$('#filename').val()+"[/img]";
+                            window.location.origin + '/uploads/' + $('#filename').val() + "[/img]";
                     }
                     var input = $("#postfield");
                     var len = input.val().length;
@@ -57,7 +57,7 @@ $(document).ready(function() {
                     input[0].setSelectionRange(len, len);
                     document.getElementById("submitbutton").disabled = false;
                 }
-                else if(error !== 'none' && typeof error !== 'undefined'){
+                else if (error !== 'none' && typeof error !== 'undefined') {
                     alert(error);
                     document.getElementById("submitbutton").disabled = false;
                 }
@@ -67,11 +67,11 @@ $(document).ready(function() {
     });
 });
 
-function updateFilename(){
+function updateFilename() {
     temp = $('#upload').val().split(".");
-    extension = temp[temp.length-1];
-    $('#filename').val(window.performance.now().toString().replace('.','') +
-                       '.' + extension);
+    extension = temp[temp.length - 1];
+    $('#filename').val(window.performance.now().toString().replace('.', '') +
+        '.' + extension);
     document.getElementById("submitbutton").disabled = true;
     $('#uploadsubmit').click();
 };
