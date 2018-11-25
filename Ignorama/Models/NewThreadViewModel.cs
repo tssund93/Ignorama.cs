8ae7a90deaf1676db8e466fbc4fba819d366a720
Travis <tssund93@gmail.com>
2018-11-25 02:50:55 -0600

    made new thread page look nice

--- Ignorama/Models/NewThreadViewModel.cs
+++ Ignorama/Models/NewThreadViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,9 +8,15 @@ namespace Ignorama.Models
 {
     public class NewThreadViewModel
     {
+        [Required]
         public string Title { get; set; }
+
+        [Required]
         public string Text { get; set; }
+
+        [Required]
         public int TagID { get; set; }
+
         public IEnumerable<Tag> Tags { get; set; }
     }
 }
