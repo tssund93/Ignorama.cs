234dfbb935f6672fbedb90651dc88ae4ef56d8b6
Travis Sunderland <travis@sunderland.io>
2018-11-25 23:35:22 -0600

    add interface for tables with user and IP to reduce code duplication

---
+++ Ignorama/Models/IUserIP.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Ignorama.Models
+{
+    public interface IUserIP
+    {
+        User User { get; set; }
+        string IP { get; set; }
+    }
+}
