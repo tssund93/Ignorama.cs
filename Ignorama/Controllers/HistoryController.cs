954265766df8956226dcf8327793c13f044079ba
Travis <tssund93@gmail.com>
2018-11-28 17:09:27 -0600

    history pages

---
+++ Ignorama/Controllers/HistoryController.cs
@@ -0,0 +1,95 @@
+﻿using Ignorama.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Ignorama.Controllers
+{
+    public class HistoryController : Controller
+    {
+        private readonly ForumContext _context;
+        private readonly UserManager<User> _userManager;
+
+        public HistoryController(ForumContext context, UserManager<User> userManager)
+        {
+            _context = context;
+            _userManager = userManager;
+        }
+
+        [Authorize(Roles = "Moderator")]
+        [HttpGet("/History/ByUser/{userID}")]
+        public IActionResult ByUser(long userID)
+        {
+            return View("ByUser", _context.Users.Find(userID).UserName);
+        }
+
+        [Authorize(Roles = "Moderator")]
+        [HttpGet("/History/ByIP/{ip}")]
+        public IActionResult ByIP(string ip)
+        {
+            return View("ByIP", ip);
+        }
+
+        [Authorize(Roles = "Moderator")]
+        [HttpGet("/History/GetUserPosts/{userID}")]
+        public IActionResult GetUserPosts(int userID)
+        {
+            var user = _userManager.GetUserAsync(HttpContext.User).Result;
+            var roles = Util.GetRoles(user, _userManager);
+
+            return new OkObjectResult(
+                _context.Posts
+                    .Where(post => post.User.Id == userID)
+                    .OrderBy(post => post.Time)
+                    .Select(post => new
+                    {
+                        post.ID,
+                        Highlighted = false,
+                        post.Anonymous,
+                        User = post.User,
+                        IP = post.IP,
+                        post.RevealOP,
+                        post.Bump,
+                        post.Time,
+                        post.Text,
+                        Locked = true,
+                        Seen = false,
+                        Roles = roles
+                    }));
+        }
+
+        [Authorize(Roles = "Moderator")]
+        [HttpGet("/History/GetIPPosts/{userIP}")]
+        public IActionResult GetIPPosts(string userIP)
+        {
+            var user = _userManager.GetUserAsync(HttpContext.User).Result;
+            var roles = Util.GetRoles(user, _userManager);
+
+            return new OkObjectResult(
+                _context.Posts
+                    .Where(post => post.IP == userIP)
+                    .OrderBy(post => post.Time)
+                    .Select(post => new
+                    {
+                        post.ID,
+                        Highlighted = false,
+                        post.Anonymous,
+                        User = post.User,
+                        IP = post.IP,
+                        post.RevealOP,
+                        post.Bump,
+                        post.Time,
+                        post.Text,
+                        Locked = true,
+                        Seen = false,
+                        Roles = roles
+                    }));
+        }
+    }
+}
---
+++ Ignorama/wwwroot/js/ip-history.js
@@ -0,0 +1,24 @@
+﻿var userIP = function () {
+    var url = window.location.href.replace(/\/$/, '');
+    return url.substr(url.lastIndexOf('/') + 1).replace(/#.*$/, '');
+}();
+
+var userHistoryVue = new Vue({
+    el: 'main',
+    data: {
+        posts: [],
+        page: 1,
+        perPage: 20,
+    },
+    created: function () {
+        this.getPosts(userIP);
+    },
+    methods: {
+        getPosts: function (userIP) {
+            axios.get('/History/GetIPPosts/' + userIP)
+                .then(response => {
+                    this.posts = response.data;
+                });
+        },
+    },
+});
