8f1d4982be48376324fbc234d9ebae3c9f7c1e18
Travis Sunderland <travis@sunderland.io>
2018-11-29 15:45:05 -0600

    add ban table

---
+++ Ignorama/Migrations/20181129214418_AddBanTable.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Ignorama.Migrations
+{
+    public partial class AddBanTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Bans",
+                columns: table => new
+                {
+                    ID = table.Column<long>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    UserId = table.Column<long>(nullable: true),
+                    IP = table.Column<string>(nullable: true),
+                    Reason = table.Column<string>(nullable: false),
+                    PostID = table.Column<int>(nullable: false),
+                    EndTime = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Bans", x => x.ID);
+                    table.ForeignKey(
+                        name: "FK_Bans_Posts_PostID",
+                        column: x => x.PostID,
+                        principalTable: "Posts",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Bans_AspNetUsers_UserId",
+                        column: x => x.UserId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Bans_PostID",
+                table: "Bans",
+                column: "PostID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Bans_UserId",
+                table: "Bans",
+                column: "UserId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Bans");
+        }
+    }
+}
