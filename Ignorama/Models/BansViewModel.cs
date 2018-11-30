053aca3a9d9e2a17df38ffea317400ec74df8f67
Travis <tssund93@gmail.com>
2018-11-29 22:33:15 -0600

    view user's ban log

---
+++ Ignorama/Models/BansViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Ignorama.Models
+{
+    public class BansViewModel
+    {
+        public IEnumerable<Ban> Bans { get; set; }
+        public Post Post { get; set; }
+    }
+}
