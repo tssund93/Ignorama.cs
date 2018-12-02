1880928e07f830646085bc624bfcff847c20d3b4
Travis Sunderland <travis@sunderland.io>
2018-12-02 03:03:22 -0600

    reports dashboard

--- Ignorama/wwwroot/js/site.js
+++ Ignorama/wwwroot/js/site.js
@@ -1,6 +1,9 @@
-﻿function banUser(postID, banReasonID, event) {
+﻿function banUser(postID, banReasonID, event, callback) {
     event.preventDefault();
-    $.post("/Bans/New/" + postID + "/" + banReasonID);
+    $.post("/Bans/New/" + postID + "/" + banReasonID)
+        .done(() => {
+            if (callback) callback();
+        });
 }
 
 function reportUser(postID, banReasonID, event) {
