fa54dd8a632077e9b3e5d4950ebc9540f7907677
Travis Sunderland <travis@sunderland.io>
2018-12-02 02:27:13 -0600

    can report posts

---
+++ Ignorama/Models/Report.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Ignorama.Models
+{
+    public class Report : IUserIP
+    {
+        public long ID { get; set; }
+
+        [Required]
+        public Post Post { get; set; }
+
+        public User User { get; set; }
+
+        public string IP { get; set; }
+
+        [Required]
+        public BanReason Reason { get; set; }
+
+        [Required]
+        public bool Active { get; set; }
+    }
+}
